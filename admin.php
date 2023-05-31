c160448312b95c936728c7481f3f16e297ee00a8
Jhon Escalona <116379627+ScalonaStudioPlus@users.noreply.github.com>
2023-05-31 23:27:21 +0200

    initial commit

---
+++ admin.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<meta charset="utf-8" />
+		<title>Infocentro Escuque</title>
+		<meta name="description" content="overview &amp; stats" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+        <link rel="stylesheet" href="assets/css/style.css"
+		<!-- bootstrap & fontawesome -->
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="assets/font-awesome/4.5.0/css/font-awesome.min.css" />
+        <link rel="stylesheet" href="/activos/css/style.css">
+		<!-- page specific plugin styles -->
+
+		<!-- text fonts -->
+		<link rel="stylesheet" href="assets/css/fonts.googleapis.com.css" />
+
+		<!-- ace styles -->
+		<link rel="stylesheet" href="assets/css/ace.min.css" class="ace-main-stylesheet" id="main-ace-style" />
+
+		<!--[if lte IE 9]>
+			<link rel="stylesheet" href="assets/css/ace-part2.min.css" class="ace-main-stylesheet" />
+		<![endif]-->
+		<link rel="stylesheet" href="assets/css/ace-skins.min.css" />
+		<link rel="stylesheet" href="assets/css/ace-rtl.min.css" />
+
+		<!--[if lte IE 9]>
+		  <link rel="stylesheet" href="assets/css/ace-ie.min.css" />
+		<![endif]-->
+
+		<!-- inline styles related to this page -->
+
+		<!-- ace settings handler -->
+		<script src="assets/js/ace-extra.min.js"></script>
+
+		<!-- HTML5shiv and Respond.js for IE8 to support HTML5 elements and media queries -->
+
+		<!--[if lte IE 8]>
+		<script src="assets/js/html5shiv.min.js"></script>
+		<script src="assets/js/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body class="no-skin">
+		<div id="navbar" class="navbar navbar-default          ace-save-state">
+			<div class="navbar-container ace-save-state" id="navbar-container">
+				<button type="button" class="navbar-toggle menu-toggler pull-left" id="menu-toggler" data-target="#sidebar">
+					<span class="sr-only">Toggle sidebar</span>
+
+					<span class="icon-bar"></span>
+
+					<span class="icon-bar"></span>
+
+					<span class="icon-bar"></span>
+				</button>
+
+				<div class="navbar-header pull-left">
+					<a href="admin.php" class="navbar-brand">
+						<small>
+							<i class="fa fa-leaf"></i>
+							Infocentro Escuque
+						</small>
+					</a>
+				</div>
+								<li>
+									<a href="#" class="perfil">
+										<i class="ace-icon fa fa-user"></i>
+										Perfil 
+									</a>
+								</li>
+
+								<li class="divider"></li>
+
+								<li>
+									<a href="#" class="salir">
+										<i class="ace-icon fa fa-power-off"></i>
+										Salir
+									</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</div><!-- /.navbar-container -->
+		</div>
+		
+    <!--Header - Menu-->
+    <header>
+        <div class="container__header">
+            <div class="logo">
+                <a href="#">
+                    <img src="assets/images/infoncentro.png"  alt="">
+                </a>
+            </div>
+
+            <div class="menu">
+                <nav>
+                    <ul>
+                        <li><a href="#">Inicio</a></li>
+						<li><a href="#">Historia</a></li>
+						<li><a href="#">Razon Social</a></li>
+                        <li><a href="#">Organizaciones vinculadas al Proyecto</a></li>
+                    </ul>
+                </nav>
+
+                <a href="#" class="btn__quote">Necesidades del Contexto</a>
+
+            </div>
+        </div>
+    </header>
+
+    <main>
+
+<!--INICIO-->
+        <!--Portada de inicio-->
+        <div class="container__cover div__offset">
+            <div class="cover">
+                <section class="text__cover">
+                    <h1>Infoncentro Escuque</h1>
+                    <p>Son puntos de encuentro comunitarios. Donde podemos acceder a las tecnologías de la información. 
+						Son salas equipadas con computadoras personales interconectadas para dar libre acceso a Internet..</p>
+                    <a href="#" class="btn__text-cover btn__text">Empezar</a>
+                </section>
+                <section class="image__cover">
+                    <img src="assets/images/portada.png" alt="">
+                </section>
+            </div>
+        </div>
+    <!--Razón Social-->
+
+	<div class="container__trust container__card-primary">
+            <div class="trust card__primary">
+                <div class="text__trust text__card-primary">
+                    <p>GENERA CONFIANZA PRIMERO</p>
+                    <h1>Razon Social<br>
+				         Infoncentro Escuque
+				   </h1>
+                </div>
+                <div class="container__trust container__box-cardPrimary">
+                    <div class="card__trust box__card-primary">
+                        <img src="assets/images/Trust area/anchor.png" alt="">
+                        <h2>Misión</h2>
+                        <p>la misión del Infocentro de Escuque es ser una institución
+                          donde las personas puedan encontrar acceso a internet para aquellos que no
+                          lo poseen y dar apoyo a los estudiantes a realizar sus actividades.</p>
+                    </div>
+                    <div class="card__trust box__card-primary">
+                        <img src="assets/images/Trust area/archive.png" alt="">
+                        <h2>Visión</h2>
+                        <p>El Infocentro de Escuque tiene como visión volver a ser una
+                        institución como lo era antes e incluso mejor, ya que debido a la falta de
+                         equipos suficientes por el daño de algunos de ellos..</p>
+                    </div>
+                    <div class="card__trust box__card-primary">
+                        <img src="assets/images/Trust area/user.png" alt="">
+                        <h2>Satisfacción del usuario garantizada</h2>
+                        <p>La pagina Web es creada con la intención de sastifacer las Necesidades
+							del usuario </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+          <!--Historia-->
+       <div class="container__about div__offset">
+		<div class="about">
+			<div class="text__about">
+				<h1>Historia De La Organizacion</h1>
+				<p>El Infocentro de Escuque fue fundado en el 2009,
+					 aunque el proyecto de los Infocentro ha estado en funcionamiento desde el año 2001 y
+					 ha sido una instalación donde las personas pueden ir a buscar información mediante la tecnología</p>
+					 <a href="#" class="btn__text-about btn__text">Sáber Mas</a>
+			</div>
+			<div class="image_about">
+				<img src="assets/images/organigrama.png"  alt="">
+				<img src="assets/images/mapa.png" alt="">
+			</div>
+		</div>
+	   </div>
+  
+	  <!--Organizaiones vinuladas al proyeto-->
+	  <div class="container__about div__offset">
+		<div class="about">
+			<div class="text__about">
+				<h1>Organizaciones Vinculadas Al Proyecto</h1>
+				<p>•	El Infocentro trabaja por el “Ministerio del Poder Popular para la Ciencias y Tecnología”<br>
+                   •    Comunidad de Escuque<br>
+                   •	Estudiantes de informática<br>
+                   •	Universidad Politécnica Territorial del Estado Trujillo “Mario Briceño Iragorry”<br>
+</p>
+					 <a href="#" class="btn__text-about btn__text">Sáber Mas</a>
+			</div>
+			<div class="image_about">
+				<img src="assets/images/ministerio.png"  alt="">
+				<img src="assets/images/uptt.png" alt="">
+			</div>
+		</div>
+	   </div>
+	   </main>
+
+	   <!--Necesidades Del Contexto-->
+	   <div class="container__service container__card-primary div__offset">
+		<div class="service card__primary">
+			<div class="text__service text__card-primary">
+				<p>Descripción del diagnóstico</p>
+				<h1>Nosotros los alumnos de Informática del Trayecto II
+				 hemos realizado una inspección a las instalaciones del Infocentro de Escuque 
+				 y hemos observado el estado de la instalación y con ayuda de un encargado que nos proporcionó información sobre la situación del Infocentro. </h1>
+			</div>
+			<div class="container__card-service container__box-cardPrimary">
+				<div class="card__service box__card-primary">
+					<img src="assets/images/objetivos.png"  width="300px">
+					<h2>Árbol de objetivos</h2>
+			</div>
+			<div class="container__card-service container__box-cardPrimary">
+				<div class="card__service box__card-primary">
+					<img src="assets/images/cuadro.png"  width="300px">
+					<h2>Alternatinas De Soluciones</h2>
+				</div>
+				<div class="container__card-service container__box-cardPrimary">
+				<div class="card__service box__card-primary">
+					<img src="assets/images/arbol.png"  width="300px">
+					<h2>Jerarquización e identificación de necesidades</h2>
+				</div>
+			
+		</div>
+	   </div>
+	   </div>
+	</body>
+</html>
